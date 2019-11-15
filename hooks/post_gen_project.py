1a4114826d802eb78da6a885ccfd367928686b5c
Patrick Rainsberry <patrick.rainsberry@autodesk.com>
2019-11-15 10:45:40 -0800

    added post gen hook to initialize git and add submodule

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+subprocess.call(['git', 'submodule', 'add', 'https://github.com/tapnair/apper'])
+
