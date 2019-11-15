69628eaab95dabdb2b5928ccdd9f58c595a7eea9
Patrick Rainsberry <patrick.rainsberry@autodesk.com>
2019-11-15 14:11:36 -0800

    updated id 5

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,24 @@
 import subprocess
+import os
+import uuid
+import json
+
 
 if __name__ == '__main__':
     subprocess.call(['git', 'init'])
     subprocess.call(['git', 'add', '*'])
     subprocess.call(['git', 'commit', '-m', 'Initial commit'])
     subprocess.call(['git', 'submodule', 'add', 'https://github.com/tapnair/apper'])
+
+    CURDIR = os.path.realpath(os.path.curdir)
+
+    file_path = os.path.join(CURDIR, '{{ cookiecutter.addin_name }}.manifest')
+    with open(file_path, 'r+') as f:
+        text = f.read()
+        manifest = json.loads(text)
+        manifest['id'] = str(uuid.uuid4())
+        f.seek(0)
+        f.write(json.dumps(manifest))
+        f.truncate()
+
 
